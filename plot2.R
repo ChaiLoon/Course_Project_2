3e6fc8e25d0d0138f99eca0266e60d597152d5ae
ChaiLoon <shinnystar.cl@gmail.com>
2020-12-29 23:50:59 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,5 +1,5 @@
 # Have total emissions from PM2.5 decreased in the
-# Baltimore City, Maryland (\color{red}{\verb|fips == "24510"|}fips == "24510") 
+# Baltimore City, Maryland (fips == "24510") 
 # from 1999 to 2008? Use the base plotting system to make a plot answering this question.
 
 # Load required libraries
